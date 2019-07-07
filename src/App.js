297ddbecd02a92e35f56ddd1a7a98e149b3426b5
Hila Bitterman <hilabitterman@Hilas-MBP.fritz.box>
2019-07-07 10:19:38 +0200

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    topStories:[]
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+      .then(({ data }) => {
+
+        const topStoriesIds = data.slice(0, 10);
+
+        topStoriesIds
+          .map(storyId => {
+            const storyEndpoint = `https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`;
+            axios.get(storyEndpoint).then(response => {
+              this.setState({topStories: response.data})
+            })
+            return this.state;
+        })
+        console.log(this.state.topStories);
+        })
+          .catch(error =>
+            console.log(error));
+  }
+
+
+  render() {
+      const {topStories} = this.state
+      const {id, title, by, url} = topStories;
+    return (
+        <div>
+        {Object.keys(topStories).map((story, id) =>
+            (
+                <ul>
+                <li key={id}>
+                {url}
+                </li>
+                </ul>
+            )
+        )}
+        </div>
+
+    );
 }
+}
+
+
 
 export default App;
