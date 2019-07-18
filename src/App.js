4899d1b5302696bf232affc6b10b470e7d9eceaa
Hila Bitterman <hilabitterman@Hilas-MBP.fritz.box>
2019-07-18 19:13:44 +0200

    story array is rendering beautifully

--- src/App.js
+++ src/App.js
@@ -3,52 +3,67 @@ import "./App.css";
 import axios from "axios";
 
 class App extends React.Component {
-  state = {
-    topStories:[]
-  };
+    state = {
+        topStories:[]
+    };
 
-  componentDidMount() {
-    axios
-      .get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
-      .then(({ data }) => {
+    componentDidMount() {
+        axios
+            .get("https://hacker-news.firebaseio.com/v0/topstories.json")
+            .then(({ data }) => {
+                const topStoriesIds = data.slice(0, 10);
 
-        const topStoriesIds = data.slice(0, 10);
+                topStoriesIds.map(storyId => {
+                    const storyEndpoint = `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`;
+                    axios.get(storyEndpoint).then(response => {
+                        const { id, title, by, url } = response.data;
+                        const { topStories } = this.state;
+                        this.setState({
+                            topStories: [
+                                ...topStories,
+                                {
+                                    id: id,
+                                    title: title,
+                                    by: by,
+                                    url: url
+                                }
+                            ]
+                        });
+                        // console.log(this.state.topStories);
+                        // const {topStories} = this.state
 
-        topStoriesIds
-          .map(storyId => {
-            const storyEndpoint = `https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`;
-            axios.get(storyEndpoint).then(response => {
-              this.setState({topStories: response.data})
+                        // const story = topStories.map(story => story)
+                        // console.log("story", story);
+                    });
+
+                    return this.state;
+                });
             })
-            return this.state;
-        })
-        console.log(this.state.topStories);
-        })
-          .catch(error =>
-            console.log(error));
-  }
+            .catch(error => console.log(error));
+    }
+
+    render() {
+        // const id = this.state.topStories.id
+        const {topStories} = this.state;
 
 
-  render() {
-      const {topStories} = this.state
-      const {id, title, by, url} = topStories;
-    return (
-        <div>
-        {Object.keys(topStories).map((story, id) =>
-            (
-                <ul>
-                <li key={id}>
-                {url}
-                </li>
-                </ul>
-            )
-        )}
+        //   console.log("object.keys", Object.keys(this.state.topStories[keys]));
+        //   const topStories = Object.keys(this.state.topStories).map((keys) => {
+        //       console.log(this.state.topStories[keys])
+        //
+        // })
+        // console.log(topStories);
+
+        return <div>
+        {topStories.map((story) => (
+            <li key={story.id}>
+             by: {story.by} 
+         title: {story.title}
+            website: {story.url}
+            </li>
+        ))}
         </div>
-
-    );
+    }
 }
-}
-
-
 
 export default App;
