07fb5f59f705cbb4a92f557cff6977fe248e8e5a
Hila Bitterman <hilabitterman@Hilas-MacBook-Pro.local>
2019-08-21 12:03:59 +0200

    added flexbox changes and some font cohesivity

--- src/App.js
+++ src/App.js
@@ -3,75 +3,61 @@ import "./App.css";
 import axios from "axios";
 import styled from "styled-components";
 
-
-
-const Header = styled.div`
-    max-width:756px;
-    padding-left: 12px;
-    padding-right: 12px;
-    padding-top: 12px;
-    line-height: 1.0em;
-    font-weight: 500;
-    margin: 15px 0px;
-    position: relative;
-    left: 162px;
-
+const Page = styled.div`
+    display: flex;
+    min-height: 100vh;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
 `;
-
+const Container = styled.div`
+    flex-direction: column;
+`;
+const Header = styled.div`
+    padding-top: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    line-height: 1em;
+    font-weight: 500;
+    font-family: "-apple-system,BlinkMacSystemFont,Helvetica,sans-serif";
+`;
 const SiteName = styled.div`
     background-color: #f7df1e;
     padding: 4px 8px;
-    float: left;
 `;
-
 const Body = styled.div`
-    display: flex;
-    padding: 50px;
-    max-width:756px;
-    font-family: '-apple-system,BlinkMacSystemFont,Helvetica,sans-serif';
+    font-family: "-apple-system,BlinkMacSystemFont,Helvetica,sans-serif";
 `;
-
-const Container = styled.div`
-    max-width:756px;
-`
-
 
 const StoryList = styled.div`
     background-color: #f8f7f5;
-    max-width:756px;
-    padding: opx 15px;
-    display: flex;
-    flex-direction: column;
-
 `;
 
 const Story = styled.section`
     margin-top: 28px;
     margin-bottom: 28px;
-    max-width:570px;
-    padding: opx 15px;
-
-
-`
+    max-width: 570px;
+    padding: 0px 15px;
+    justify-self: center;
+`;
 const Atag = styled.a`
-    text-decoration:none;
-    color: #3366AA;
+    text-decoration: none;
+    color: #3366aa;
     border-bottom-width: 1px;
     font-size: 20px;
     line-height: 1.4em;
-`
+    border-bottom-style: solid;
+    border-bottom-color: #ddd;
+`;
 const Author = styled.p`
     font-size: 14px;
-    color: #5A5A5A;
+    color: #5a5a5a;
     margin-top: 4px;
     margin-bottom: 0.8em;
     text-transform: uppercase;
     line-height: 1.2em;
-
-`
-
-
-
+`;
 
 class App extends React.Component {
     state = {
@@ -111,23 +97,23 @@ class App extends React.Component {
     render() {
         const { topStories } = this.state;
         return (
-            <div>
-                <Header>
-                    <SiteName>Hacker News</SiteName>
-                </Header>
-                <Body>
-                    <Container>
+            <Page>
+                <Container>
+                    <Header>
+                        <SiteName>Hacker News</SiteName>
+                    </Header>
+                    <Body>
                         <StoryList>
                             {topStories.map(story => (
                                 <Story key={story.id}>
-                                <Atag href = {story.url}>{story.title}</Atag>
-                                <Author>{story.by}</Author>
+                                    <Atag href={story.url}>{story.title}</Atag>
+                                    <Author>{story.by}</Author>
                                 </Story>
                             ))}
                         </StoryList>
-                    </Container>
-                </Body>
-            </div>
+                    </Body>
+                </Container>
+            </Page>
         );
     }
 }
